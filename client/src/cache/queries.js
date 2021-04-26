ca3aacbf2cb43a3c0767e2b0d34d6479fbbaa8ed
Kalos <k2323973530@gmail.com>
2021-04-26 15:50:05 -0400

    Account finish

---
+++ client/src/cache/queries.js
@@ -0,0 +1,30 @@
+import { gql } from "@apollo/client";
+
+export const GET_DB_USER = gql`
+	query GetDBUser {
+		getCurrentUser {
+			_id
+			name
+			email
+		}
+	}
+`;
+
+export const GET_DB_TODOS = gql`
+	query GetDBTodos {
+		getAllTodos {
+			_id
+			name
+			owner
+			items {
+				_id
+				description
+				due_date
+				assigned_to
+				completed
+			}
+			sortRule
+			sortDirection
+		}
+	}
+`;
